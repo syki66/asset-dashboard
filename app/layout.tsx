316936cfb023edb31d5a22a948e8e440b8849a03
syki66 <66syki@gmail.com>
2025-02-21 23:50:34 +0900

    env: sonner 설치 및 적용

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from 'next';
 import localFont from 'next/font/local';
 import './globals.css';
 import Providers from '@/utils/providers/providers';
+import { Toaster } from 'sonner';
 
 const geistSans = localFont({
   src: './fonts/GeistVF.woff',
@@ -30,6 +31,7 @@ export default function RootLayout({
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         <Providers>{children}</Providers>
+        <Toaster expand={true} position="top-center" richColors />
       </body>
     </html>
   );
