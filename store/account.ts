7e1c44e9d6c48c6439ff2175aae318c58aa17a6d
syki66 <66syki@gmail.com>
2025-08-20 06:37:07 +0000

    feat: 금리테이블 활용을 위한 스토어 추가

--- store/account.ts
+++ store/account.ts
@@ -1,5 +1,6 @@
 import { AccountProps, DashboardProps } from '@/types';
 import { create } from 'zustand';
+import { rateTable } from '@/constants/keywords';
 
 // 계좌 데이터 상태 관리
 interface AccountState {
@@ -47,4 +48,15 @@ export const useDetailToggleStore = create<DetailToggleState>((set) => ({
   showDetail: false,
   toggleDetail: () => set((state) => ({ showDetail: !state.showDetail })),
   setShowDetail: (show) => set({ showDetail: show }),
+}));
+
+// 금리 테이블 상태 관리
+interface InterestRateState {
+  interestRates: { date: string; interestRate: number }[];
+  setInterestRates: (rates: { date: string; interestRate: number }[]) => void;
+}
+
+export const useInterestRateStore = create<InterestRateState>((set) => ({
+  interestRates: rateTable,
+  setInterestRates: (rates) => set({ interestRates: rates }),
 }));
