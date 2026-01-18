0288cb113b488e86cc01610a58e4a68b6bae3bd8
syki66 <66syki@gmail.com>
2026-01-18 23:47:49 +0900

    env: 테스트 코드 작성을 위한 jest 설치

---
+++ jest.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+  roots: ['<rootDir>'],
+  testMatch: ['**/__tests__/**/*.test.ts', '**/__tests__/**/*.test.tsx'],
+  moduleNameMapper: {
+    '^@/(.*)$': '<rootDir>/$1',
+  },
+};
